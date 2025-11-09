9dc2fcf8a5d99ac9df381eec33d7216870e02622
LucasMatsubara <lucasmatsubara11@gmail.com>
2025-11-08 23:43:58 -0300

    feat: implementação de data para usuários conforme type definido.

---
+++ Bytecode_HC/src/data/usuarios.ts
@@ -0,0 +1,6 @@
+import { Usuario } from "../types/usuario";
+
+export const usuarios: Usuario[] = [
+  { id: 1, nome: "Ana", email: "ana@hc.com", senha: "1234", tipo: "paciente" },
+  { id: 2, nome: "Dr. João", email: "joao@hc.com", senha: "abcd", tipo: "medico" },
+];
