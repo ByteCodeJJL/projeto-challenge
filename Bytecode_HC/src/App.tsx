5c81a05c7279a525bb1ef71a6abd74e468fee384
Jppcamilo <jpcamilopereira@gmail.com>
2025-11-09 12:23:07 -0300

    style: Adiconei os roots de estilos que estamos utilizando no APP.tsx .

--- Bytecode_HC/src/App.tsx
+++ Bytecode_HC/src/App.tsx
@@ -5,14 +5,29 @@ import './globals.css'
 
 export default function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div
+      className="
+        flex flex-col min-h-screen 
+        bg-[var(--cor-fundo-geral)] 
+        text-[var(--cor-texto-principal)] 
+        font-sans
+      "
+    >
       <Header />
-      
-      <main className="flex-grow">
+
+      <main
+        id="conteudo-principal"
+        className="
+          flex-grow 
+          focus:outline-none 
+          scroll-smooth
+        "
+        tabIndex={-1}
+      >
         <Outlet />
       </main>
-      
+
       <Footer />
     </div>
-  )
+  );
 }
