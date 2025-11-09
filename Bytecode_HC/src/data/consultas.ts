fd54ec3fc72a7fd638cd6c0ef295c5686e12edf7
Jppcamilo <jpcamilopereira@gmail.com>
2025-11-09 12:27:47 -0300

    feat: Adicionei em uma das funcionalidades de API na pasta data em consultas.ts .

--- Bytecode_HC/src/data/consultas.ts
+++ Bytecode_HC/src/data/consultas.ts
@@ -1,25 +1,3 @@
 import { Consulta } from "../types/consulta";
-import { pacientes } from "./pacientes";
 
-export const consultas: Consulta[] = [
-  {
-    id: 1,
-    paciente: pacientes[0],
-    pacienteId: 1,
-    medicoId: 1,
-    data: "2025-11-05",
-    horario: "10:00",
-    tipo: "teleconsulta", 
-    status: "marcada",     
-  },
-  {
-    id: 2,
-    paciente: pacientes[1],
-    pacienteId: 2,
-    medicoId: 2,
-    data: "2025-11-06",
-    horario: "15:30",
-    tipo: "presencial",
-    status: "realizada", 
-  },
-];
+export const consultas: Consulta[] = [];
