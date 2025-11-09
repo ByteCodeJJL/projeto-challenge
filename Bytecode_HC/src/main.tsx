d559e829a0806ed9ce76cd1f332af8881cee0a48
Jppcamilo <jpcamilopereira@gmail.com>
2025-11-09 12:24:49 -0300

    feat: Adicionei uma nova funcionalidade chamada detalhesConsultas ndentro da main.tsx .

--- Bytecode_HC/src/main.tsx
+++ Bytecode_HC/src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
 import App from './App'
+import { AuthProvider } from './contexts/AuthProvider';
 import './globals.css'
 
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Acesso from './routes/Acesso/index';
 import Calendario from './routes/Calendario/index';
 import Contato from './routes/Contato/index';
@@ -13,27 +15,31 @@ import FAQ from './routes/FAQ/index';
 import Sobre from './routes/Sobre/index';
 import Teleconsulta from './routes/Teleconsulta/index';
 import Erro from './routes/Error/index';
+import DetalhesConsulta from "./routes/DetalhesConsulta";
 
 const router = createBrowserRouter([
   {
-    path: "/", element: <App />,errorElement: <Erro/> ,children: [
-      {path: "/", element: <Home />},
-      {path: "/acesso", element: <Acesso />},
-      {path: "/calendario", element: <Calendario />},
-      {path: "/contato", element: <Contato />},
-      {path: "/sobre", element: <Sobre />},
-      {path: "/teleconsulta", element: <Teleconsulta />},
-      {path: "/integrantes", element: <Integrantes />},
-      {path: "/faq", element: <FAQ />}
-    ]
-  }
-])
+    path: "/",
+    element: <App />,
+    errorElement: <Erro />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/acesso", element: <Acesso /> },
+      { path: "/calendario", element: <Calendario /> },
+      { path: "/contato", element: <Contato /> },
+      { path: "/integrantes", element: <Integrantes /> },
+      { path: "/faq", element: <FAQ /> },
+      { path: "/sobre", element: <Sobre /> },
+      { path: "/teleconsulta", element: <Teleconsulta /> },
+      { path: "/detalhes-consulta/:id", element: <DetalhesConsulta />},
+    ],
+  },
+]);
 
-
-
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
