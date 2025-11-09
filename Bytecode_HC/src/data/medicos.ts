e3c62022e796778d934c119f49213fa03fff0bc2
LucasMatsubara <lucasmatsubara11@gmail.com>
2025-11-08 23:44:42 -0300

    feat: implementação de data para os médicos de acordo com o type definido.

---
+++ Bytecode_HC/src/data/medicos.ts
@@ -0,0 +1,77 @@
+
+import { Medico } from "../types/medico";
+
+export const medicos: Medico[] = [
+  {
+    id: 1,
+    nome: "Dr. João Silva",
+    especialidade: "Cardiologia",
+    crm: "12345-SP",
+  },
+  {
+    id: 101,
+    nome: "Dra. Beatriz Costa",
+    especialidade: "Cardiologia",
+    crm: "11223-SP",
+  },
+
+  {
+    id: 2,
+    nome: "Dra. Maria Santos",
+    especialidade: "Dermatologia",
+    crm: "67890-SP",
+  },
+  {
+    id: 102,
+    nome: "Dr. Roberto Almeida",
+    especialidade: "Dermatologia",
+    crm: "22334-SP",
+  },
+
+  {
+    id: 103,
+    nome: "Dra. Ana Pereira",
+    especialidade: "Pediatria",
+    crm: "33445-SP",
+  },
+  {
+    id: 104,
+    nome: "Dr. Lucas Fernandes",
+    especialidade: "Pediatria",
+    crm: "44556-SP",
+  },
+
+  {
+    id: 105,
+    nome: "Dra. Sofia Oliveira",
+    especialidade: "Ginecologia",
+    crm: "55667-SP",
+  },
+  {
+    id: 106,
+    nome: "Dr. Mateus Rodrigues",
+    especialidade: "Ginecologia",
+    crm: "66778-SP",
+  },
+
+  {
+    id: 107,
+    nome: "Dr. Carlos Lima",
+    especialidade: "Ortopedia",
+    crm: "77889-SP",
+  },
+  {
+    id: 108,
+    nome: "Dra. Juliana Moreira",
+    especialidade: "Ortopedia",
+    crm: "88990-SP",
+  },
+
+  {
+    id: 109,
+    nome: "Dra. Laura Mendes",
+    especialidade: "Oftalmologia",
+    crm: "99001-SP",
+  },
+
+];
